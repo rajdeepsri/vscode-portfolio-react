36357305cabdac3336a72dc8e4e29dc4a015095c
Rajdeep Shrivastava <rajdeep1999.srivastav@gmail.com>
2023-08-09 00:49:22 +0530

    created file structure

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,23 @@
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { Home, Contact, Education, Projects, Skills, Error } from "./pages";
+import { Sidebar, Navbar, Footer } from "./components";
+
 const App = () => {
-  return <></>;
+  return (
+    <BrowserRouter>
+      <Sidebar />
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/contact" element={<Contact />} />
+        <Route path="/education" element={<Education />} />
+        <Route path="/projects" element={<Projects />} />
+        <Route path="/skills" element={<Skills />} />
+        <Route path="*" element={<Error />} />
+      </Routes>
+      <Footer />
+    </BrowserRouter>
+  );
 };
 
 export default App;
---
+++ src/pages/index.js
@@ -0,0 +1,7 @@
+export { default as Contact } from "./Contact/Contact";
+export { default as Education } from "./Education/Education";
+export { default as Error } from "./Error/Error";
+export { default as Github } from "./Github/Github";
+export { default as Home } from "./Home/Home";
+export { default as Projects } from "./Projects/Projects";
+export { default as Skills } from "./Skills/Skills";
